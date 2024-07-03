9deddb2a3c32e774783b0349a75cf108a440c18d
Laxman Kumar <laxmankumarvashisth@gmail.com>
2024-07-03 15:03:02 +0530

    IMP_Beginning With Transact SQL

---
+++ Password_Generation.sql
@@ -0,0 +1,14 @@
+
+select concat(substring(sailor_id,1,3), right(sailor_name,3)) as PASSWORD from table name order by .... 
+
+Parameter Values
+Parameter	Description
+string	Required. The string to extract from
+start	Required. The start position. The first position in string is 1
+length	Required. The number of characters to extract. Must be a positive number
+
+
+
+string  = "laxman";
+right -- used two parameter anme of th estring and how many element u want form last.  right(sailor,3) -- man
+right uased toqew take name frim the first 3 name =----  left(sailor_name, 3) -- lax
